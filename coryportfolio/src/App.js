a9bfa418ee78c763b4e511c2095536986d6466ef
Cory Fabian <Coryf65@gmail.com>
2021-09-12 17:21:04 -0500

    Added particles into main page

--- coryportfolio/src/App.js
+++ coryportfolio/src/App.js
@@ -1,10 +1,30 @@
 import './App.css';
 import Navigation from './screens/navigationBar/NavigationBar'
+import Particles from 'react-particles-js';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
+      <Particles
+        params={{
+          "particles": {
+              "number": {
+                  "value": 50
+              },
+              "size": {
+                  "value": 2
+              }
+          },
+          "interactivity": {
+              "events": {
+                  "onhover": {
+                      "enable": true,
+                      "mode": "repulse"
+                  }
+              }
+          }
+	}} />
     </div>
   );
 }

bab8396165f33b4be3ac52326959cbd0886fb5d1
Cory Fabian <Coryf65@gmail.com>
2021-09-12 12:27:20 -0500

    Added BootstrapReact and Setting up base colors and Nav

--- coryportfolio/src/screens/navigationBar/NavigationBar.js
+++ coryportfolio/src/screens/navigationBar/NavigationBar.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import { navigationLinks } from '../../helpers/navigationLinks'
+
+function createLinks() {
+    return navigationLinks.map((e, index) => (
+        <Nav.Link key={index} href={e.ref}>{e.name}</Nav.Link>
+    ))
+}
 
 function NavigationBar() {
     return (
         <div>
-            navigation
+            <div id="home">
+                <Navbar expand="md">
+                <Navbar.Brand href="#home">Cory Fabian</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse>
+                    <Nav>
+                        {createLinks()}
+                    </Nav>
+                </Navbar.Collapse>
+                </Navbar>
+            </div>
         </div>
     )
 }

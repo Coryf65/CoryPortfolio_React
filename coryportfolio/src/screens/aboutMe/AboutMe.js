99601f57dfd65b9f14679ca5f9ec2e5cc4d6c779
Cory Fabian <Coryf65@gmail.com>
2021-09-14 21:37:07 -0500

    New images

---
+++ coryportfolio/src/screens/aboutMe/AboutMe.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './aboutMe.css'
+import me from '../../images/me_circle.png'
+
+function AboutMe() {
+    return (
+        <div className='about-container' id='about-me'>
+            <div>
+                <img src={me} id='my-image' alt="A picture of Cory Fabian, headshot smiling with a background of water." />
+            </div>
+            <div className='about-text'>
+                <h1>About Me</h1>
+                <p>Making cool software is my passion! Software is challenging and rewarding due to the effect it
+                    has on others. Creating ideas into a reality, to benefit others, is what drives me. Striving towards
+                    making the codebase and tooling extraordinary excites me. Learning from others, and teaching
+                    others, allows us to do anything as a team.
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default AboutMe

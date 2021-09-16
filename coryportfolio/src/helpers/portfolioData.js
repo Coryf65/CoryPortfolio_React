40e50a56b11a83cc1dad0329e14fc3da7423a05f
Cory Fabian <Coryf65@gmail.com>
2021-09-16 18:55:17 -0500

    adding new portfolio section

---
+++ coryportfolio/src/helpers/portfolioData.js
@@ -0,0 +1,14 @@
+import factorioMod from '../images/projectImages/SquidInk-FactorioMods.png'
+// Add more, using the same from the factorio mod
+
+const portfolioData = [
+    {
+        image: factorioMod,
+        link: 'https://mods.factorio.com/mod/SquidInk',
+        title: 'Squid Ink a Factorio Mod',
+        summary: 'A mod that I made for fun to make silly machines and an easier time playing!',
+        tech: 'Lua, Factorio API'
+    }
+]
+
+export default portfolioData
